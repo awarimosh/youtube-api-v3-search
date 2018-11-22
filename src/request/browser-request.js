f31077dd66eaec34c994d261018caaa165fe7d3c
mosh <moshood@tractive.com.my>
2018-11-22 18:54:23 +0800

    changes to request

--- src/request/browser-request.js
+++ src/request/browser-request.js
@@ -4,7 +4,7 @@ module.exports = function(url){
     return new Promise((resolve, reject)=> {
         request(url, function (error, response, body) {
             if (!error && response.statusCode === 200) {
-                resolve(body);
+                resolve(JSON.parse(body));
             }
             else{
                 reject(new Error (error) );
